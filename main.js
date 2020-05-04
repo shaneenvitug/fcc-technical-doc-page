660819b9e0e01c6e5b03c9c473ca5bdbdac7aece
Shaneen Vitug <shaneenvitug@gmail.com>
2020-05-04 22:44:08 +1000

    styling done and js function show/hide div

---
+++ main.js
@@ -0,0 +1,8 @@
+function hideShowDiv() {
+  var x = document.getElementById("show-hide");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
