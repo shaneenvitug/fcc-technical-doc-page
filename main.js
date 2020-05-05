9b4a73475e4597165224d4dbd0cd3d700bb71432
Shaneen Vitug <shaneenvitug@gmail.com>
2020-05-05 17:17:46 +1000

    footer page added

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 console.log('test');
 
 function hideShowDiv() {
-  var x = document.getElementById("show-hide");
+  const x = document.getElementById("show-hide");
   if (x.style.display === "block") {
     x.style.display = "none";
   } else {
